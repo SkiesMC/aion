872335c5e2ac940907a67722e00d10e7dfb9870b
tozydev <tozydev@nguyenthanhtan.id.vn>
2024-08-09 13:40:12 +0700

    refactor!: remove dataDirectory property

--- paperPlugin/src/main/kotlin/com/skiesmc/aion/plugin/AionKotlinPlugin.kt
+++ paperPlugin/src/main/kotlin/com/skiesmc/aion/plugin/AionKotlinPlugin.kt
@@ -3,7 +3,6 @@ package com.skiesmc.aion.plugin
 import com.skiesmc.aion.coroutines.mcCoroutine
 import kotlinx.coroutines.runBlocking
 import org.bukkit.plugin.java.JavaPlugin
-import java.nio.file.Path
 
 /**
  * Base class for Typhon Kotlin plugins.
@@ -12,8 +11,6 @@ import java.nio.file.Path
 abstract class AionKotlinPlugin :
     JavaPlugin(),
     AionPlugin {
-    final override val dataDirectory: Path = dataFolder.toPath()
-
     override suspend fun onLoadAsync() = Unit
 
     override suspend fun onEnableAsync() = Unit
