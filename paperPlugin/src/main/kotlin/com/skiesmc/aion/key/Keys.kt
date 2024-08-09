26489d132909e0a24fa7d96187ace8f44fe07334
tozydev <tozydev@nguyenthanhtan.id.vn>
2024-08-09 14:55:10 +0700

    feat: add namespacedKey extension function to Plugin
    This commit introduces an extension function named `namespacedKey` for the `Plugin` class. It allows easy creation of `NamespacedKey` instances using a `Plugin` and a value string. This enhancement simplifies working with namespaced keys within the plugin context.

--- paperPlugin/src/main/kotlin/com/skiesmc/aion/key/Keys.kt
+++ paperPlugin/src/main/kotlin/com/skiesmc/aion/key/Keys.kt
@@ -2,8 +2,12 @@ package com.skiesmc.aion.key
 
 import net.kyori.adventure.key.Key
 import org.bukkit.NamespacedKey
+import org.bukkit.plugin.Plugin
 
 /**
  * Converts this Key to a Bukkit [NamespacedKey].
  */
 fun Key.asBukkit() = if (this is NamespacedKey) this else NamespacedKey(namespace(), value())
+
+/** Creates a new [NamespacedKey] using the provided [value] and the plugin's instance as the namespace. */
+fun Plugin.namespacedKey(value: String) = NamespacedKey(this, value)
